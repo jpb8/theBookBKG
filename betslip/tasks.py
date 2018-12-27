a5a8c1e8ee4cb5b70e37a510b02502c59c06681c
Joe Ballard <ballard.joe33@gmail.com>
2018-12-27 11:46:13 -0600

    starttime fix

--- betslip/tasks.py
+++ betslip/tasks.py
@@ -8,3 +8,13 @@ def update_bet_values():
     bets = PlacedBet.objects.filter(status=0)
     for bet in bets:
         bet.update_bet_values()
+
+
+@db_periodic_task(crontab(minute='*/5', hour='0-23'))
+def update_bet_times():
+    bets = PlacedBet.objects.filter(status=0)
+    for bet in bets:
+        if bet.start_time is None:
+            bet.start_time = bet.get_earliest_start_time()
+            bet.save()
+            print("{} start time: {}".format(bet, bet.start_time))
