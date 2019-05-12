99dc19635b34673287b43076f23491a932835913
Joe Ballard <ballard.joe33@gmail.com>
2019-05-12 11:12:34 -0500

    conditional formatting

--- players/views.py
+++ players/views.py
@@ -64,29 +64,39 @@ def stack_builder(request):
 
 
 def pitching_stats(request):
-    pitchers = todays_pitchers()
+    # pitchers = todays_pitchers()
     tp_data = tp_utils()
     cols = []
     for t in tp_data:
         col = {}
         col["row"] = int(t["numb"])
-        col["var_arr"], col["opac_arr"] = opacity(10, t["min"], t["max"])
+        col["var_arr"] = opac_ranges(10, t["min"], t["max"])
+        col["opac_arr"] = opac_values(10, t["low"])
         cols.append(col)
     print(cols)
     cont_dict = {
-        'pitchers': pitchers,
+        # 'pitchers': pitchers,
         'cols': json.dumps(cols)
     }
     return render(request, "players/pitcher_stats.html", cont_dict)
 
 
-def opacity(n, minn, maxx):
+def opac_ranges(n, minn, maxx):
     diff = maxx - minn
     var_div = diff / n
-    opac_div = round(1 / n, 2)
     var_arr = []
-    opac_arr = []
-    for i in range(n - 1):
+    for i in range(n):
         var_arr.append(round(float(var_div * (i + 1) + minn), 4))
-        opac_arr.append(round(float(opac_div * (i + 1)), 2))
-    return var_arr, opac_arr
+    return var_arr
+
+
+def opac_values(n, low):
+    opac_div = round(1 / n, 2)
+    opac_arr = []
+    if low:
+        for i in range(n):
+            opac_arr.append(round(float(1 - (opac_div * i)), 2))
+    else:
+        for i in range(n):
+            opac_arr.append(round(float(opac_div * i), 2))
+    return opac_arr
